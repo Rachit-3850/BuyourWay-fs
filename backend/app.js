904b1608d53d48105edf273117a0937f387e1334
Rachit-3850 <2020ucp1736@mnit.ac.in>
2023-06-16 02:52:52 +0530

    deploying frontend

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,23 @@ const errorMiddleware = require('./middlewares/error');
 const cookieParser = require("cookie-parser")
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+
+dotenv.config({path: 'backend/config/config.env'})
 
 app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload())
+
+app.use(function (req, res, next) {
+    // console.log(process.env.FRONT_URL);
+    res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_URL);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 
 const products = require('./routes/product')
