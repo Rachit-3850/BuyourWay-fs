8b153ec829277ddccd47cf32c2fc1935815668fb
Rachit-3850 <2020ucp1736@mnit.ac.in>
2023-06-21 21:24:46 +0530

    added access control allow header

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,19 @@ const errorMiddleware = require('./middlewares/error');
 const cookieParser = require("cookie-parser")
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')
 const dotenv = require('dotenv')
 
 dotenv.config({path: 'backend/config/config.env'})
-
+app.use(cors())
+app.use("/", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(cookieParser());
